535171677d11e75e616c5b4df8967ee325418ba5
Rita Abou Fares <rja62@mail.aub.edu>
2026-06-10 13:27:17 +0300

    second

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,17 +1,15 @@
-'use client';
+"use client";
 
-import { useState } from 'react';
-import { motion } from 'framer-motion';
-import { Eye, EyeOff, Mail } from 'lucide-react';
-import BasePage from '@/components/ui/BasePage';
-import LoginPage from '@/components/ui/LoginPage';
-import RegisterPage from '@/components/ui/RegisterPage';
-import Start from '@/components/ui/Start';
+import { useState } from "react";
+import { motion } from "framer-motion";
+import { Eye, EyeOff, Mail } from "lucide-react";
+import BasePage from "@/components/ui/BasePage";
+import LoginPage from "@/components/ui/LoginPage";
+import Start from "@/components/ui/Start";
 export default function Home() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
 
-  return (
-   <Start/>  );
+  return <Start />;
 }
